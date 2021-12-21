f1646eade2553bb9371d6c0f4925992d902f8634
Pacman-v1 <41747534+Pacman-v1@users.noreply.github.com>
2021-12-21 16:09:38 +0300

    Update createSQS.py

--- createSQS.py
+++ createSQS.py
@@ -2,7 +2,7 @@ import boto3
 
 client = boto3.resource('sqs')
 
-response = client.create_queue( QueueName='rekog-sqs-cpd-s1903348', Attributes = {
+response = client.create_queue( QueueName='queue-name', Attributes = {
     'DelaySeconds': '60',
     'MessageRetentionPeriod': '1209600'
 })
